7ac6a0321852f61aea9264c152473a18d2e304ff
zyx <zhangyuxuan@cowarobot.com>
2023-05-09 01:39:28 -0700

    本地方法改远程调用

---
+++ example/callee/userservice.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include "user.pb.h"
+
+/*
+UserService本地方法改造成一个rpc服务，供远程调用
+*/
+class UserService : public zrpc::UserServiceRpc
+{
+public:
+    bool Login(std::string name, std::string pwd)
+    {
+        std::cout << "doing local service: Login" << std::endl;
+        std::cout << "name:" << name << " pwd:" << pwd << std::endl;  
+        return true;
+    }
+
+    /*
+    重写基类UserServiceRpc的虚函数 下面这些方法都是框架直接调用的
+    1. caller   ===>   Login(LoginReq)  => muduo =>   callee 
+    2. callee   ===>    Login(LoginReq)  => 交到下面重写的这个Login方法上了
+    */
+    void Login(::google::protobuf::RpcController* controller,
+                       const ::zrpc::LoginReq* request,
+                       ::zrpc::LoginAck* response,
+                       ::google::protobuf::Closure* done) override
+    {
+        //取参
+        std::string name = request->name(); 
+        std::string pwd = request->pwd();
+        
+        //调用本地方法
+        bool result = Login(name, pwd);     
+
+        //设置rsp
+        response->set_success(result);      
+        response->mutable_result()->set_result(0);
+        response->mutable_result()->set_errmsg("");
+
+        //回调，网络层发送rsp
+        done->Run();                        
+    }
+};
+
+int main()
+{
+    return 0;
+}
